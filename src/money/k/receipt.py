e0aaf35cb9dc07b26d7f60ed2f182872f266e431
lukaszk <srsrsrew@gmail.com>
2023-03-12 10:42:24 +0100

    Add some properties. Extract date.

--- src/money/k/receipt.py
+++ src/money/k/receipt.py
@@ -14,29 +14,50 @@ class Receipt:
     def __init__(
         self, receipt_path: str, city: str, poppler_path: Optional[str] = None
     ) -> None:
-        self.receipt_path = receipt_path
-        self.poppler = poppler_path
-        self.products: Optional[List[Product]] = None
-        self.total: Optional[str] = None
-        self.city = city
+        self._receipt_path = receipt_path
+        self._poppler = poppler_path
+        self._city = city
+        self._products: Optional[List[Product]] = None
+        self._total: Optional[str] = None
+        self._date: Optional[str] = None
+
+    @property
+    def products(self):
+        if self._products is None:
+            raise ValueError("Products don't exist. Did you run create_receipt?")
+        return self._products
+
+    @property
+    def total(self):
+        if self._total is None:
+            raise ValueError("Total doesn't exist. Did you run create_receipt?")
+        return self._total
+
+    @property
+    def date(self):
+        if self._total is None:
+            raise ValueError("Date doesn't exist. Did you run create_receipt?")
+        return self._total
 
     def create_receipt(self, validate: Optional[bool] = True) -> None:
         receipt_raw = self._get_raw_receipt()
 
         products = self._extract_products(receipt_raw)
         prices = self._extract_prices(receipt_raw)
+        date = self._extract_date(receipt_raw)
 
         receipt_total = prices.pop()
         if validate:
             self._validate_parsing(products, prices, receipt_total)
 
-        self.products = [
+        self._products = [
             Product(article, price) for article, price in zip(products, prices)
         ]
-        self.total = receipt_total
+        self._total = receipt_total
+        self._date = date
 
     def _get_raw_receipt(self):
-        page = convert_from_path(self.receipt_path, 500, poppler_path=self.poppler)[0]
+        page = convert_from_path(self._receipt_path, 500, poppler_path=self._poppler)[0]
         with tempfile.TemporaryDirectory() as tmpd:
             tmp_path = f"{tmpd}out.png"
             page.save(tmp_path, "PNG")
@@ -45,7 +66,7 @@ class Receipt:
     def _extract_products(self, receipt: str) -> List[str]:
         return (
             receipt.replace("\n\n", "\n")
-            .split(f"{self.city}\n")[1]
+            .split(f"{self._city}\n")[1]
             .split("Total")[0]
             .splitlines()
         )
@@ -58,6 +79,15 @@ class Receipt:
             .split("Tax")[0]
             .replace(",", ".")
             .splitlines()
+        )
+
+    @staticmethod
+    def _extract_date(receipt: str) -> str:
+        return (
+            receipt.replace("\n\n", "\n")
+            .split("Date: ")[1]
+            .split("Receipt:")[0]
+            .strip()
         )
 
     @staticmethod
@@ -78,10 +108,12 @@ class Receipt:
         return True
 
     def __repr__(self):
-        if not self.products or not self.total:
+        if not self._products or not self._total:
             return ""
-        r = "".join([str(el) + "\n" for el in self.products])
+        r = "".join([str(el) + "\n" for el in self._products])
         r += Product.REPR_SIZE * "-" + "\n"
-        empty_space = Product.REPR_SIZE - len("Total") - len(self.total)
-        r += f"Total{empty_space * ' '}{self.total}"
+        empty_space_total = Product.REPR_SIZE - len("Total") - len(self._total)
+        r += f"Total{empty_space_total * ' '}{self._total}\n"
+        empty_space_date = Product.REPR_SIZE - len("Date") - len(self._date)
+        r += f"Date{empty_space_date * ' '}{self._date}"
         return r
